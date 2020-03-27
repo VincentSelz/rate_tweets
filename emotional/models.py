4786065ac9e203b0873b79768f31c6e0e1502cfa
Vincent Selz <s6viselz@uni-bonn.de>
2020-03-27 19:12:48 +0100

    Add emotional app.

---
+++ emotional/models.py
@@ -0,0 +1,46 @@
+from otree.api import (
+    models,
+    widgets,
+    BaseConstants,
+    BaseSubsession,
+    BaseGroup,
+    BasePlayer,
+    Currency as c,
+    currency_range,
+)
+
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+def make_field(choice):
+    return models.StringField(
+        choices=choice,
+        label="",
+        widget=widgets.RadioSelectHorizontal,
+    )
+
+class Constants(BaseConstants):
+    name_in_url = 'emotional'
+    players_per_group = None
+    num_rounds = 1
+    positive =[["Negativ","Negativ"],["Neutral","Neutral"],["Positiv","Positiv"]]
+    optimistic = [["Pessimistisch","Pessimistisch"],["Neutral","Neutral"],["Optimistisch","Optimistisch"]]
+    happiness = [["Verärgert","Verärgert"],["Neutral","Neutral"],["Zufrieden","Zufrieden"]]
+    emotional = [["Sachlich","Sachlich"],["Neutral","Neutral"],["Emotional","Emotional"]]
+    choices = [positive,optimistic,happiness,emotional]
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    rating = make_field(Constants.emotional)
