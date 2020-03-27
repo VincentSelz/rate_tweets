c9eb790c35744db29d4e5f36fc4543767b46b81b
Vincent Selz <s6viselz@uni-bonn.de>
2020-03-27 19:13:24 +0100

    Add positive app.

---
+++ positive/pages.py
@@ -0,0 +1,20 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Introduction(Page):
+    def is_displayed(self):
+        return self.round_number == 1
+
+class MyPage(Page):
+    form_model = 'player'
+    form_fields = ['rating']
+
+class ExitPage(Page):
+    pass
+
+
+page_sequence = [Introduction, MyPage, MyPage, MyPage, MyPage,
+            MyPage, MyPage, MyPage, MyPage, MyPage, MyPage, ExitPage
+        ]
