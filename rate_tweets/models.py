b9f4c25d6e6d62440ad0d6a1e219d3ca7d0f16b5
Joaquin Felber <jfelberzeta@gmail.com>
2020-03-27 20:07:35 +0100

    Assigning tweets to participants and displaying them in MyPage each round

--- rate_tweets/models.py
+++ rate_tweets/models.py
@@ -9,6 +9,8 @@ from otree.api import (
     currency_range,
 )
 
+import itertools as it
+
 
 author = 'Your name here'
 
@@ -17,30 +19,76 @@ Your app description
 """
 
 
+
 class Constants(BaseConstants):
     name_in_url = 'rate_tweets'
     players_per_group = None
-    num_rounds = 1
+    num_rounds = 10
+    q_per_round = 10
 
 
 class Subsession(BaseSubsession):
-    pass
+    def get_tweets(self):
+        import csv
+
+        with open('data/example_corona_tweets.tsv', newline='') as f:
+           reader = csv.reader(f)
+           data = list(reader)
 
 
+           self.session.vars['data'] = set(map(str, data))
+
+    def set_sample(self):
+        sample = set()
+        for i in range(Constants.q_per_round):
+            try:
+                sample.add(self.session.vars['data'].pop())
+            except KeyError:
+                print('no more tweets')
+                break
+
+        index = range(len(sample))
+
+        return dict(zip(index, sample))
+
+    def set_choices_cycle(self, start_at):
+
+       choice_1 =[["Negativ","Negativ"],["Neutral","Neutral"],["Positiv","Positiv"]]
+       choice_2 = [["Pessimistisch","Pessimistisch"],["Neutral","Neutral"],["Optimistisch","Optimistisch"]]
+       choice_3 = [["Verärgert","Verärgert"],["Neutral","Neutral"],["Zufrieden","Zufrieden"]]
+       choice_4 = [["Sachlich","Sachlich"],["Neutral","Neutral"],["Emotional","Emotional"]]
+       choices = [choice_1, choice_2, choice_3, choice_4]
+       choices_cycle = it.cycle(choices)
+
+       return it.islice(choices_cycle, 0, None)
+
+
+
+    def creating_session(self):
+        self.get_tweets()
+        count = 0
+        for p in self.get_players():
+
+            #p.sample = self.set_sample()
+            p.participant.vars['sample'] = self.set_sample()
+            p.participant.vars['counter'] = 0
+            p.participant.vars['choices'] = self.set_choices_cycle(count)
+            p.participant.vars['rating'] = models.StringField(widget= widgets.RadioSelectHorizontal, label='',choices =next(p.participant.vars['choices']))
+
+            count += 1
 class Group(BaseGroup):
     pass
 
 
 class Player(BasePlayer):
 
-    rating = models.StringField(widget= widgets.RadioSelectHorizontal, label="",choices =[["Negativ","Negativ"],["Neutral","Neutral"],["Positiv","Positiv"]])
+    
+    rating1 = models.StringField(widget= widgets.RadioSelectHorizontal, label='',choices =[["Pessimistisch","Pessimistisch"],["Neutral","Neutral"],["Optimistisch","Optimistisch"]])
+
+
 #    def choices():
 #        import random
-#        choice_1 =[["Negativ","Negativ"],["Neutral","Neutral"],["Positiv","Positiv"]]
-#        choice_2 = [["Pessimistisch","Pessimistisch"],["Neutral","Neutral"],["Optimistisch","Optimistisch"]]
-#        choice_3 = [["Verärgert","Verärgert"],["Neutral","Neutral"],["Zufrieden","Zufrieden"]]
-#        choice_4 = [["Sachlich","Sachlich"],["Neutral","Neutral"],["Emotional","Emotional"]]
-#        x = str(random.randrange(1,4))
+
 #        return
 #choices()
 
--- rate_tweets/pages.py
+++ rate_tweets/pages.py
@@ -7,14 +7,26 @@ class Introduction(Page):
     def is_displayed(self):
         return self.round_number == 1
 
+
 class MyPage(Page):
-    form_model = 'player'
+    def vars_for_template(self):
+        tweets = self.participant.vars['sample']
+        print('asdfasdfasdf')
+        return dict(
+            currentTweet= tweets[self.round_number - 1],)
+
+    form_model = 'participant'
+
     form_fields = ['rating']
 
+
+
+
+
 class ExitPage(Page):
-    pass
+    def is_displayed(self):
+        return self.round_number > 10
 
 
-page_sequence = [Introduction, MyPage, MyPage, MyPage, MyPage,
-            MyPage, MyPage, MyPage, MyPage, MyPage, MyPage, ExitPage
+page_sequence = [Introduction, MyPage, ExitPage
         ]
