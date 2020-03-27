30b1fe20c56ce0c14fbc93fd572e8f3a0ca71247
Vincent Selz <s6viselz@uni-bonn.de>
2020-03-27 20:22:43 +0100

    Toggle with create_session.

--- rate_tweets/models.py
+++ rate_tweets/models.py
@@ -16,33 +16,47 @@ doc = """
 Your app description
 """
 
+def make_field(choice):
+    return models.StringField(
+        choices=choice,
+        label="",
+        widget=widgets.RadioSelectHorizontal,
+    )
 
 class Constants(BaseConstants):
     name_in_url = 'rate_tweets'
     players_per_group = None
-    num_rounds = 1
+    num_rounds = 10
+    positive =[["Negativ","Negativ"],["Neutral","Neutral"],["Positiv","Positiv"]]
+    optimistic = [["Pessimistisch","Pessimistisch"],["Neutral","Neutral"],["Optimistisch","Optimistisch"]]
+    happiness = [["Verärgert","Verärgert"],["Neutral","Neutral"],["Zufrieden","Zufrieden"]]
+    emotional = [["Sachlich","Sachlich"],["Neutral","Neutral"],["Emotional","Emotional"]]
+    choices = [positive,optimistic,happiness,emotional]
+    rating_1 = make_field(positive)
+    rating_2 = make_field(optimistic)
+    rating_3 = make_field(happiness)
+    rating_4 = make_field(emotional)
+    ratings = [rating_1,rating_2,rating_3,rating_4]
 
 
 class Subsession(BaseSubsession):
+    #def creating_session(self):
+    #    import itertools
+    #    import random
+    #    sub_ratings = Constants.ratings.copy()
+    #    if self.round_number == 1:
+    #        sub_rating = random.shuffle(sub_ratings)
+    #    rating = itertools.cycle(sub_rating)
+    #    for p in self.get_players():
+    #        p.player.vars['rating'] = next(rating)
     pass
-
 
 class Group(BaseGroup):
     pass
 
 
 class Player(BasePlayer):
-
-    rating = models.StringField(widget= widgets.RadioSelectHorizontal, label="",choices =[["Negativ","Negativ"],["Neutral","Neutral"],["Positiv","Positiv"]])
-#    def choices():
-#        import random
-#        choice_1 =[["Negativ","Negativ"],["Neutral","Neutral"],["Positiv","Positiv"]]
-#        choice_2 = [["Pessimistisch","Pessimistisch"],["Neutral","Neutral"],["Optimistisch","Optimistisch"]]
-#        choice_3 = [["Verärgert","Verärgert"],["Neutral","Neutral"],["Zufrieden","Zufrieden"]]
-#        choice_4 = [["Sachlich","Sachlich"],["Neutral","Neutral"],["Emotional","Emotional"]]
-#        x = str(random.randrange(1,4))
-#        return
-#choices()
-
-
-    #rating = models.StringField(widget= widgets.RadioSelectHorizontal, choices=[["Negativ","Negativ"],["Neutral","Neutral"],["Positiv","Positiv"]])
+    #def example(self):
+    #    rating = self.player.vars['rating']
+    #    return rating
+    rating = make_field([["Negativ","Negativ"],["Neutral","Neutral"],["Positiv","Positiv"]])
