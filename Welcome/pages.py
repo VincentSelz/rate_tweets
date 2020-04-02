2484d011592f817613974f4b733496c972306ffd
Vincent Selz <s6viselz@uni-bonn.de>
2020-04-02 11:31:35 +0200

    Remove redundant apps.

--- Welcome/pages.py
+++
@@ -1,9 +0,0 @@
-from otree.api import Currency as c, currency_range
-from ._builtin import Page, WaitPage
-from .models import Constants
-
-
-class WelcomePage(Page):
-    pass
-
-page_sequence = [WelcomePage]
