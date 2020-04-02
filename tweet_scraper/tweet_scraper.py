b9594b3273f00a17f019281f2059f443d8852e5e
Vincent Selz <s6viselz@uni-bonn.de>
2020-04-02 15:21:16 +0200

    Show number of tweet in filename.

--- tweet_scraper/tweet_scraper.py
+++ tweet_scraper/tweet_scraper.py
@@ -47,6 +47,6 @@ def get_df_of_tweets(datafile):
                print('this tweet cannot be displayed.')
                pass
        df = pd.DataFrame(tweets)
-       return df.to_csv('data/html_{}'.format(datafile), index=False)
+       return df.to_csv('data/_html'+str(len(tweets))+'_{}'.format(datafile), index=False)
 
 get_df_of_tweets("test_data.csv")
